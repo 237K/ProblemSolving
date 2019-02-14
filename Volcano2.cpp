2a1c6af6bac648aa53373128de33739c89aba8f4
237K <237ref@gmail.com>
2019-02-14 09:05:31 +0900

    [시뮬레이션(DFS,BFS)] <화산폭발2> DFS로 case를 순회하면서 BFS를 돌리는 형태로 시뮬레이션을 구현함

--- Volcano2.cpp
+++ Volcano2.cpp
@@ -13,6 +13,15 @@
 //	5. 초원 파괴가 최소화 되는 경우의 파괴되지 않은 초원 개수를 출력
 //
 
+//	초원에 바리게이트를 설치하는 경우(는 DFS로 조합함)안에서 BFS(용암이 초원 파괴해나감)를 돌리는 방식으로 구현함
+//	{DFS(BFS)} 형태
+//
+//	(*Core Function)
+//	1. Graph::void Simulation(int) -> DFS(Recursion), 바리게이트를 설치하는 모든 경우의 수를 순회
+//	2. Graph::void Eruption() -> BFS(queue), 용암이 흘러가면서 초원 파괴
+//
+
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -32,7 +41,7 @@ private:
 	vector<PII> Volcano;	//입력받을 때 화산의 좌표를 저장해둠
 	queue<PII> Lava;		//용암이 흘러가는 과정을 BFS로 구현하기 위함
 	vector<PII> Grass;		//입력받을 때 초원의 좌표를 저장해둠
-	bool **Check;			//
+	bool **Check;			//DFS돌릴 때 사용하기 위함
 	int Unaffected;
 public:
 	Graph(int _Size, int _Gate) : Size(_Size), NumOfGate(_Gate), Unaffected(0)
@@ -50,6 +59,7 @@ public:
 			}
 		}
 		Volcano.clear();
+		Grass.clear();
 		while (!Lava.empty()) { Lava.pop(); }
 	}
 	~Graph()
@@ -57,77 +67,80 @@ public:
 		for (int del = 0; del < Size; ++del)
 		{
 			delete[] Map[del];
+			delete[] Check[del];
 		}
 		delete[] Map;
+		delete[] Check;
 	}
-	void Setting(ifstream& _fin)
+	void Setting(ifstream& _fin)					//초기 맵 상태 인풋값 읽어오는 함수
 	{
 		for (int r = 0; r < Size; r++)
 		{
 			for (int c = 0; c < Size; c++)
 			{
 				_fin >> Map[r][c];
-				if (Map[r][c] == 0)
+				if (Map[r][c] == 0)					//초기 격자의 상태가 0이면
 				{
-					Grass.push_back(PII(r, c));
+					Grass.push_back(PII(r, c));		//해당 좌표를 Grass 벡터에 넣음
 				}
-				if (Map[r][c] == 2)
+				if (Map[r][c] == 2)					//초기 격자의 상태가 2이면
 				{
-					Volcano.push_back(PII(r, c));
+					Volcano.push_back(PII(r, c));	//해당 좌표를 Volcano 벡터에 넣음
 				}
 			}
 		}
 	}
-	void Simulation(int depth)
-	{
-		cout << "depth : " << depth << endl;
-		if (depth == NumOfGate + 1)
-		{
-			//int ThisTime = 0;
-			//Eruption();
-			//ThisTime = CountArea();
-			//if (Unaffected > ThisTime)
-			//	Unaffected = ThisTime;
-			//cout << "Unaffected Area : " << Unaffected << endl << endl;
+	void Simulation(int depth)						//(*Core Function) 초원에 바리케이트를 설치하는 모든 경우의 수를 순회
+	{												
+		if (depth == NumOfGate + 1)					//탈출 조건
+		{											//인풋값으로 주어진 바리게이트수만큼 설치하면
+			int ThisTime = 0;
+			Eruption();								//화산폭발
+			//PrintMap();							//(제대로 시뮬레이션 되고 있는지 확인하기 위함)
+			ThisTime = CountArea();					//파괴되지 않은 초원이 기존값보다 많으면 갱신
+			if (Unaffected < ThisTime)
+				Unaffected = ThisTime;				
+			ClearMap();								//맵을 초기값으로 초기화
 			return;
 		}
 		else
-		{
+		{																					//탐색 조건
 			for (vector<int>::size_type i = 0; i < Grass.size(); ++i)
 			{
 				PII CurGrass = Grass[i];
-				cout << "(" << CurGrass.first << ", " << CurGrass.second << ") ";
 				if (Check[CurGrass.first][CurGrass.second] == false)
 				{
+					//cout << "(" << CurGrass.first << ", " << CurGrass.second << ") ";		//모든 경우의 수를 탐색하는지 확인하기 위함
 					Check[CurGrass.first][CurGrass.second] = true;
 					Map[CurGrass.first][CurGrass.second] = 1;
 					Simulation(depth + 1);
 					Check[CurGrass.first][CurGrass.second] = false;
 					Map[CurGrass.first][CurGrass.second] = 0;
+					//cout << endl;
 				}
 			}
 		}
 	}
-	void Eruption()
+	void Eruption()														//(*Core Function) 용암이 흘러가면서 초원을 파괴하는 함수
 	{
-		for (vector<PII>::size_type i = 0; i < Volcano.size(); i++)
+		for (vector<PII>::size_type i = 0; i < Volcano.size(); i++)		//화산의 좌표를 저장해둔 벡터에서 좌표를 하나씩 꺼내서
 		{
-			Lava.push(Volcano[i]);
+			Lava.push(Volcano[i]);										//큐에 푸쉬
 		}
 		while (!Lava.empty())
 		{
-			PII CurLoc = Lava.front();
-			Lava.pop();
-			Destroy(CurLoc);
+			PII CurLoc = Lava.front();									//임시변수에 화산좌표를 넣고
+			Lava.pop();													//큐에서 팝하고
+			Destroy(CurLoc);											//Destroy함수 호출
 		}
 	}
-	void Destroy(PII _Loc)
+	void Destroy(PII _Loc)														//(*Core Function)
 	{
 		//북쪽
-		if (_Loc.first - 1 >= 0 && Map[_Loc.first - 1][_Loc.second] == 0)
+		if (_Loc.first - 1 >= 0 && Map[_Loc.first - 1][_Loc.second] == 0)		//좌표가 맵을 벗어나지 않고, 초원이면
 		{
-			Lava.push(PII(_Loc.first - 1, _Loc.second));
-			Map[_Loc.first - 1][_Loc.second] = 3;
+			Lava.push(PII(_Loc.first - 1, _Loc.second));						//해당 좌표를 큐에 푸쉬하고
+			Map[_Loc.first - 1][_Loc.second] = 3;								//파괴되었다는 것을 표시함
 		}
 		//동쪽
 		if (_Loc.second + 1 < Size && Map[_Loc.first][_Loc.second + 1] == 0)
@@ -148,7 +161,7 @@ public:
 			Map[_Loc.first][_Loc.second - 1] = 3;
 		}
 	}
-	int CountArea()
+	int CountArea()								//파괴되지 않은 초원 개수를 반환하는 함수
 	{
 		int Recent = 0;
 		for (int r = 0; r < Size; ++r)
@@ -160,6 +173,33 @@ public:
 			}
 		}
 		return Recent;
+	}
+	void PrintAnswer() const
+	{
+		cout << "Unaffected Area : " << Unaffected << endl << endl;
+	}
+	void ClearMap()
+	{
+		for (int r = 0; r < Size; ++r)
+		{
+			for (int c = 0; c < Size; ++c)
+			{
+				if (Map[r][c] == 3)
+					Map[r][c] = 0;
+			}
+		}
+	}
+	void PrintMap() const						//(제대로 시뮬레이션 되고 있는지 확인하기 위함)
+	{
+		for (int r = 0; r < Size; ++r)
+		{
+			for (int c = 0; c < Size; ++c)
+			{
+				cout << Map[r][c] << ' ';
+			}
+			cout << endl << endl;
+		}
+		cout << endl;
 	}
 };
 
@@ -184,6 +224,7 @@ int main(void)
 		graph[tc]->Setting(fin);
 		cout << "#" << tc << endl;
 		graph[tc]->Simulation(1);
+		graph[tc]->PrintAnswer();
 	}
 
 	fin.close();
