b986aaee4bf12754f7321d540b48e2d9cabb66d2
237K <237ref@gmail.com>
2019-02-16 11:15:08 +0900

    [DP] <징검다리 건너기>

--- SteppingStone.cpp
+++ SteppingStone.cpp
@@ -13,54 +13,40 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
-#include <vector>
-#include <stack>
+#include <algorithm>
 using namespace std;
-const static int MAX = 10000;
+const static int MAX = 10001;
 int N;
 int Score[MAX] = { 0, };
-bool Check[MAX] = { false, };
-int Answer;
-stack<int> Stack;
-
-void DFS(int _N)
-{
-	Stack.push(2);
-	Stack.push(3);
-	Check[1] = true;
-	while (!Stack.empty())
-	{
-		int CurStone = Stack.top();
-		Stack.pop();
-		for (int i = 3; i <= N; ++i)
-		{
-			if (Check[i + 1] == false)
-			{
-				Check[i + 1] = true;
-				Stack.push(i + 1);
-			}
-			if (Check[i + 2] == false)
-			{
-				Check[i + 2] = true;
-				Stack.push(i + 2);
-			}
-		}
-	}
-}
+int Stone[MAX][2] = { 0, };
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
+	int Answer;
 	freopen("testcase_SteppingStone.txt", "r", stdin);
 	scanf("%d", &T);
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
+		Answer = -1;
 		scanf("%d", &N);
 		for (int i = 1; i <= N; ++i)
-		{
-			scanf("%d", &Score[i]);
+		{											
+			scanf("%d", &Score[i]);					
+			if (i == 1)
+			{
+				Stone[i][1] = Score[i];						//Stone[i][1]은 i-1번째 돌에서 한 칸 건너 i번째 징검돌에 온 경우를 의미합니다.
+				Stone[i][2] = Score[i];						//Stone[i][2]는 i-2번째 돌에서 두 칸 건너 i번째 징검돌에 온 경우를 의미합니다.
+			}
+			else
+			{
+				Stone[i][1] = Stone[i - 1][2] + Score[i];							//세 칸을 연속으로 이동할 수 없으므로, i-1번째 돌에서 한 칸 건너 i돌로 온 경우 i-1번 돌에는 2칸 건너 왔어야만 합니다. 
+				Stone[i][2] = max(Stone[i - 2][1], Stone[i - 2][2]) + Score[i];		//i-2번째 돌에서 i번 돌로 온 경우에는 i-2번 돌에 한 칸 건너 왔었을 수도 있고, 두 칸 건너 왔을 수도 있습니다.
+			}																		//그 중에 최대값을 선택해 더해줍니다.
 		}
+		Answer = max(Stone[N][1], Stone[N][2]);
+		printf("#%d %d\n", test_case, Answer);
 	}
 	return 0;
 }
