1b24247f524fda8aae9014775c26ea9b2e130137
237K <237ref@gmail.com>
2019-02-18 22:10:52 +0900

    [구현] <뒤집어 더하기>

---
+++ ReverseAdd.cpp
@@ -0,0 +1,56 @@
+//
+//	OS Windows
+//	2019.02.18
+//
+//	[Algorithm Problem Solving]
+//
+//	<뒤집어 더하기>
+//	
+//	1. 10,000,000 이하의 양수가 주어짐
+//	2. 수를 뒤집어서 더함
+//	3. 더했을 때 펠린드롬인지 확인하여 yes or no 출력
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int arr[10];
+int ReverseN;
+
+void Reverse(int n)
+{
+	int Cur = n;
+	while (1)
+	{
+		int Quotient = Cur / 10;
+		int Remainder = Cur % 10;
+		ReverseN += Remainder;
+		Cur = Cur / 10;
+		if (Cur == 0)
+			break;
+		else
+			ReverseN *= 10;
+	}
+	cout << "Number : " << n << ", Reverse : " << ReverseN << endl;
+}
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	int N;
+	freopen("testcase_ReverseAdd.txt", "r", stdin);
+	scanf("%d", &T);
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		*arr = { 0, };
+		ReverseN = 0;
+		scanf("%d", &N);
+		cout << "#" << test_case << endl;
+		Reverse(N);
+	}
+	return 0;
+}
