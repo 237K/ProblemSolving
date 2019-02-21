46f16091759d1b9d1ebb55abf7da8211a609016a
237K <237ref@gmail.com>
2019-02-21 11:04:10 +0900

    [SAMSUNG SWEA] <#6959> (D4) VER0.2 각 숫자를 모두 합한 후 10으로 나는 몫을 count하고, 다시 몫과 나머지를 더하여 10보다 크면 재귀호출, 10보다 작으면 count에 따라 승자 출력

--- SAMSUNG_SWEA#6959_ver2.cpp
+++ SAMSUNG_SWEA#6959_ver2.cpp
@@ -18,16 +18,21 @@
 //			5) 합을 10으로 나눈 몫 만큼 승자가 바뀜 (몫 : 0, 그대로 A)
 //			6) 합을 10으로 나눈 몫과 나머지의 합이 10보다 크면 10보다 작을 때 까지 반복함
 //
-//	*그러므로 이 방법에선 몇 자리 수인지 확인하는 것이 우선
-//	1) while문으로 몫이 0이 될 때까지 10으로 나누면서 Count++ 해서 확인할 수 도 있고,
-//	2) string으로 받아와서 length 쓸 수도 있음
-//	string으로 받아오는 방법으로 진행해봄
+//
+//	(*주요 흐름도)
+//	1. 1000개 이하의 수를 한 줄 단위로 읽어와서 char배열에 저장함 (getline 사용하니 채점서버에서 자꾸 파일의 마지막 숫자 하나를 안읽어와서 scanf 사용함)
+//	2. 숫자가 몇개인지 확인
+//	3. char배열에 저장된 숫자를 하나씩 int형으로 변환해서 누적하여 Sum에 저장함
+//	4. Sum을 재귀함수(DFS 함수)로 보낼건데, 숫자 개수가 짝수개이면 cnt를 0으로 해서 보내고, 홀수개이면 1로 해서 보냄
+//	5. Sum을 10으로 나눈 몫만큼 cnt를 더함
+//	6. Sum의 각 자리를 더함(Accumulate 함수)
+//	7. Sum의 각 자리를 더한게 10보다 작으면 cnt에 따라 승자 출력
+//	8. 10이상이면 다시 DFS로 보냄
+//	9. 10으로 나눈 몫만큼 cnt를 더하고.. 다시 각 자리를 더해서 10보다 작으면 출력하고 10이상이면 반복...
 //
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
-#include <iostream>
-#include <string>
 using namespace std;
 
 int Accumulate(int Num)									//각 자리 숫자를 모두 합하는 함수
@@ -48,55 +53,96 @@ int Accumulate(int Num)									//각 자리 숫자를 모두 합하는 함수
 
 void DFS(int Num, int cnt)
 {	
-	int Quotient = 0, Remainder = 0;
+	int Acc = 0;
 	cnt += Num / 10;
-	Quotient = Num / 10;
-	Remainder = Num % 10;
-	if (Quotient + Remainder < 10)
+	Acc = Accumulate(Num);
+	if (Acc < 10)
 	{
 		if (cnt % 2 == 0)
 			printf("A\n");
 		else
 			printf("B\n");
 		return;
-		
 	}
-	else if (Quotient + Remainder >= 10)
+	else if (Acc >= 10)
 	{
-		DFS(Accumulate(Quotient + Remainder), cnt);
+		DFS(Acc, cnt);
+	}
+}
+
+int StoI(char ch)
+{
+	switch (ch)
+	{
+	case 48:
+		return 0;
+		break;
+	case 49:
+		return 1;
+		break;
+	case 50:
+		return 2;
+		break;
+	case 51:
+		return 3;
+		break;
+	case 52:
+		return 4;
+		break;
+	case 53:
+		return 5;
+		break;
+	case 54:
+		return 6;
+		break;
+	case 55:
+		return 7;
+		break;
+	case 56:
+		return 8;
+		break;
+	case 57:
+		return 9;
+		break;
+	default:
+		return -1;
+		break;
 	}
 }
 
 int main(int argc, char** argv)
 {
-	string Input;
-	int Number;
 	int Sum;
 	int Length;
 	int test_case;
 	int T;
+	char Input[1001];
 	freopen("s_input6959.txt", "r", stdin);
-	scanf("%d\n", &T);
+	scanf("%d", &T);
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
-		Input = { NULL, };											//string 배열 초기화
-		Number = 0; Length = 0;	Sum = 0;							//변수 초기화
-		getline(cin, Input);										//한 줄 string으로 읽어옴 (string.length로 몇자리 수인지 확인하기 위함)
-		Number = stoi(Input);										//string을 숫자로 바꿔서 저장하고
-		Length = Input.length();									//몇 자리 수인지 저장하고
+		for (int init = 0; init < 1001; ++init)
+			Input[init] = NULL;
+		Length = 0;
+		Sum = 0;
+		scanf("%1000s", &Input);
+		while (Input[Length] != NULL)
+		{
+			Sum += StoI(Input[Length]);
+			Length++;
+		}
 		printf("#%d ", test_case);
 		if (Length == 1)
 			printf("B\n");
 		else if (Length % 2 == 0)					//주어진 수의 개수가 짝수이면
 		{
-			Sum = Accumulate(Number);				//숫자를 모두 합하고
 			DFS(Sum, 0);
 		}
 		else
 		{											//주어진 수의 개수가 홀수이면
-			Sum = Accumulate(Number);
 			DFS(Sum, 1);
 		}
+
 	}
 	return 0;
 }
