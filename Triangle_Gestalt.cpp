bce1381521a88188bff1b3d0f19a0cbf60186016
237K <237ref@gmail.com>
2019-03-14 11:39:40 +0900

    [구현] 삼각형 형태 검증

--- Triangle_Gestalt.cpp
+++ Triangle_Gestalt.cpp
@@ -5,7 +5,8 @@
 //	[Algorithm Problem Solving]
 //
 //	<삼각형 형태 알아내기>
-//		1. 꼭짓점 3개의 좌표를 입력받아서 직각삼각형인지(1 출력), 둔각삼각형인지(2 출력), 예각삼각형인지(3 출력), 삼각형이 되지 않는지(0) 출력
+//		1. 꼭짓점 3개의 좌표를 입력받아서
+//		2. 직각삼각형인지(1 출력), 둔각삼각형인지(2 출력), 예각삼각형인지(3 출력), 삼각형이 되지 않는지(0) 출력
 //
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
@@ -24,7 +25,7 @@ private:
 	PII Triangle3;
 	vector<double> Distance;
 public:
-	Test() { Distance.clear(); };
+	Test() : Triangle1(PII(0, 0)), Triangle2(PII(0, 0)), Triangle3(PII(0, 0)) { Distance.clear(); };
 	~Test() {};
 	void MakeTriangle(int x1, int y1, int x2, int y2, int x3, int y3)
 	{
@@ -47,28 +48,33 @@ public:
 			Triangle2.first == Triangle3.first && Triangle2.second == Triangle3.second ||
 			Triangle1.first == Triangle2.first == Triangle3.first ||
 			Triangle1.second == Triangle2.second == Triangle3.second ||
-			abs(Triangle1.first - Triangle1.second) == abs(Triangle2.first - Triangle2.second) == abs(Triangle3.first - Triangle3.second))
+			Triangle1.first == Triangle1.second && Triangle2.first == Triangle2.second && Triangle3.first == Triangle3.second)
 			return 0;
 
-		double D1 = GetDistance(Triangle1, Triangle2);
-		double D2 = GetDistance(Triangle1, Triangle3);
-		double D3 = GetDistance(Triangle2, Triangle3);
-
-		Distance.push_back(D1);
-		Distance.push_back(D2);
-		Distance.push_back(D3);
-
-		sort(Distance.begin(), Distance.end());
-		double Max = Distance[2];
-		double A = Distance[0];
-		double B = Distance[1];
-
-		if ((A + B) == Max)
-			return 1;
-		else if ((A + B) < Max)
-			return 2;
 		else
-			return 3;
+		{
+			double D1 = GetDistance(Triangle1, Triangle2);
+			double D2 = GetDistance(Triangle1, Triangle3);
+			double D3 = GetDistance(Triangle2, Triangle3);
+
+			Distance.push_back(D1);
+			Distance.push_back(D2);
+			Distance.push_back(D3);
+
+			sort(Distance.begin(), Distance.end());
+			double Max = Distance[2];
+			double A = Distance[0];
+			double B = Distance[1];
+
+			cout << Max << ' ' << A << ' ' << B << endl << endl;
+
+			if ((A + B) == Max)
+				return 1;
+			else if ((A + B) < Max)
+				return 2;
+			else
+				return 3;
+		}
 	}
 };
 
