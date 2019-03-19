c1c77dbe4598fb5aa3175846b9465b7ed394c306
237K <237ref@gmail.com>
2019-03-19 21:01:35 +0900

    [SAMSUNG SWEA] <#5656> (모의)

--- SAMSUNG_SWEA#5656.cpp
+++ SAMSUNG_SWEA#5656.cpp
@@ -9,248 +9,38 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
 #include <iostream>
-#include <stack>
-#include <queue>
+#include <string>
 using namespace std;
 
 typedef pair<int, int> PII;
 
+const static int MAX_BALL = 4;
 const static int MAX_ROW = 15;
-const static int MAX_COLUMN = 12;
-const static int MAX = 2147000000;
+const static int MAX_COL = 12;
+const static int MAX_BRICKS = MAX_ROW * MAX_COL;
+const static int DIRECT = 4;
+const static int DIR[DIRECT][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
 
-static bool Check[MAX_ROW][MAX_COLUMN];
-static int Map[MAX_ROW][MAX_COLUMN];
-static int Map_copy[MAX_ROW][MAX_COLUMN];
-static int Break[MAX_ROW][MAX_COLUMN];
-static int Target_Col;
-static int Target_Row;
-static queue<PII> Q;
-static stack<PII> Stack;
-static int N, Row, Col;
-static int Result;
-static int Min_cnt;
-static int Min_cnt_value;
-
-void MapCopy()
+class Test
 {
-	for (int r = 0; r < Row; ++r)
+public:
+	int Num_Ball;
+	int Num_Brick_Col;
+	int Num_Brick_Row;
+	int Map[MAX_BALL][MAX_ROW][MAX_COL];
+	int Input_Num_Brick;
+	int Min_Remainder_Brick;
+public:
+	Test(const int N, const int C, const int R)
+		: Num_Ball(N)
+		, Num_Brick_Col(C)
+		, Num_Brick_Row(R)
+		, Input_Num_Brick(0)
+		, Min_Remainder_Brick(MAX_BRICK)
 	{
-		for (int c = 0; c < Col; ++c)
-		{
-			Map_copy[r][c] = Map[r][c];
-		}
+		(void)memset(&Map[0][0][0], 0, sizeof(Map));
 	}
-}
-
-void MapInit()
-{
-	for (int r = 0; r < Row; ++r)
-	{
-		for (int c = 0; c < Col; ++c)
-		{
-			Map[r][c] = Map_copy[r][c];
-		}
-	}
-}
-
-void BreakInit()
-{
-	for (int r = 0; r < Row; ++r)
-	{
-		for (int c = 0; c < Col; ++c)
-		{
-			Break[r][c] = -1;
-		}
-	}
-}
-
-int Count()
-{
-	int C = 0;
-	for (int r = 0; r < Row; ++r)
-	{
-		for (int c = 0; c < Col; ++c)
-		{
-			if (Map[r][c] > 0)
-				C++;
-		}
-	}
-	return C;
-}
-
-void Print()
-{
-	for (int r = 0; r < Row; ++r)
-	{
-		for (int c = 0; c < Col; ++c)
-		{
-			cout << Map[r][c] << ' ';
-		}
-		cout << endl << endl;
-	}
-	cout << "Result : " << Result << endl << endl;
-}
-
-void Drop()
-{
-	for (int c = 0; c < Col; ++c)
-	{
-		int cnt = Row - 1;
-		for (int r = Row - 1; r >= 0; --r)
-		{
-			if (Map[r][c] != 0)
-			{
-				if (Map[cnt][c] == 0)
-				{
-					Map[cnt][c] = Map[r][c];
-					Map[r][c] = 0;
-				}
-				else
-				{
-					Map[cnt][c] = Map[r][c];
-				}
-				cnt--;
-			}
-		}
-	}
-}
-
-void Bomb(int shot_row, int shot_col)
-{
-	cout << "(" << shot_row << ", " << shot_col << "), n : " << N << endl << endl;
-
-	Q.push(PII(shot_row, shot_col));
-
-	while (!Q.empty())
-	{
-		PII cur = Q.front();
-		int range = Map[cur.first][cur.second]-1;
-		Q.pop();
-		if (range < 0)
-			continue;
-
-		Map[cur.first][cur.second] = 0;
-
-		if (range == 0)
-			continue;
-			for (int i = 1; i <= range; ++i)
-			{
-				if (cur.first + i < Row)
-				{
-					if (Map[cur.first + i][cur.second] == 1)
-					{
-						Map[cur.first + i][cur.second] = 0;
-					}
-					else if (Map[cur.first + i][cur.second] > 1)
-					{
-						Q.push(PII(cur.first + i, cur.second));
-					}
-				}
-
-				if (cur.first - i >= 0)
-				{
-					if (Map[cur.first - i][cur.second] == 1)
-					{
-						Map[cur.first - i][cur.second] = 0;
-					}
-					else if (Map[cur.first - i][cur.second] > 1)
-					{
-						Q.push(PII(cur.first - i, cur.second));
-					}
-				}
-
-				if (cur.second + i < Col)
-				{
-					if (Map[cur.first][cur.second + i] == 1)
-					{
-						Map[cur.first][cur.second + i] = 0;
-					}
-					else if (Map[cur.first][cur.second + i] > 1)
-					{
-						Q.push(PII(cur.first, cur.second + i));
-					}
-				}
-
-				if (cur.second - i >= 0)
-				{
-					if (Map[cur.first][cur.second - i] == 1)
-					{
-						Map[cur.first][cur.second - i] = 0;
-					}
-					else if (Map[cur.first][cur.second - i] > 1)
-					{
-						Q.push(PII(cur.first, cur.second - i));
-					}
-				}
-			}
-	}
-	Drop();
-}
-
-bool FindTarget(int n)
-{
-	Min_cnt = MAX;
-	Min_cnt_value = MAX;
-	BreakInit();
-	bool isTarget = false;
-
-	for (int c = 0; c < Col; ++c)
-	{
-		int cnt = 0;
-		for (int r = 0; r < Row; ++r)
-		{
-			if (Map[r][c] == 1)
-			{
-				cnt++;
-			}
-			else if (Map[r][c] > 1)
-			{
-				if (cnt <= n)
-				{
-					Min_cnt = Min_cnt > cnt ? cnt : Min_cnt;
-					Break[r][c] = cnt;
-					isTarget = true;
-					return true;
-				}
-			}
-		}
-	}
-	return isTarget;
-}
-
-void Shot(int n)
-{
-	if (n > N)
-	{
-		Print();
-
-		int temp_cnt = Count();
-		Result = Result > temp_cnt ? temp_cnt : Result;
-		return;
-	}
-	else
-	{
-		for (int r = 0; r < Row; ++r)
-		{
-			for (int c = 0; c < Col; ++c)
-			{
-				if (Map[r][c] > 0 && Check[r][c] == false)
-				{
-					if (r > 0 && Map[r - 1][c] == 0)
-					{
-						Check[r][c] = true;
-						MapCopy();
-						Bomb(r, c);
-						Shot(n + 1);
-						MapInit();
-						Check[r][c] = false;
-					}
-				}
-			}
-		}
-	}
-}
+};
 
 int main(int argv, char** argc)
 {
@@ -267,7 +57,7 @@ int main(int argv, char** argc)
 		scanf("%d %d %d", &N, &Col, &Row);
 		for (int r = 0; r < MAX_ROW; ++r)
 		{
-			for (int c = 0; c < MAX_COLUMN; ++c)
+			for (int c = 0; c < MAX_COL; ++c)
 			{
 				Map[r][c] = 0;
 				Map_copy[r][c] = 0;
