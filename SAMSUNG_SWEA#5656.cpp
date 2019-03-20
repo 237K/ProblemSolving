02509b8f4e87e9c5b1539897eb1ceb710ce73e1e
237K <237ref@gmail.com>
2019-03-20 11:50:28 +0900

    [SAMSUNG SWEA] <#5656> (모의)

--- SAMSUNG_SWEA#5656.cpp
+++ SAMSUNG_SWEA#5656.cpp
@@ -7,16 +7,17 @@
 //	SAMSUNG SW Expert Academy [#5656] <벽돌 깨기> (모의 SW 역량테스트)
 //	
 #define _CRT_SECURE_NO_WARNINGS
+//#pragma GCC optimize("O3")
 #include <cstdio>
 #include <iostream>
-#include <string>
+#include <cstring>
 using namespace std;
 
 typedef pair<int, int> PII;
 
-const static int MAX_BALL = 4;
-const static int MAX_ROW = 15;
-const static int MAX_COL = 12;
+const static int MAX_BALL = 5;
+const static int MAX_ROW = 16;
+const static int MAX_COL = 13;
 const static int MAX_BRICKS = MAX_ROW * MAX_COL;
 const static int DIRECT = 4;
 const static int DIR[DIRECT][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
@@ -31,41 +32,40 @@ public:
 	int Input_Num_Brick;
 	int Min_Remainder_Brick;
 public:
-	Test(const int N, const int C, const int R)
-		: Num_Ball(N)
-		, Num_Brick_Col(C)
-		, Num_Brick_Row(R)
+	Test(const int _N, const int _W, const int _H)
+		: Num_Ball(_N)
+		, Num_Brick_Col(_W)
+		, Num_Brick_Row(_H)
 		, Input_Num_Brick(0)
-		, Min_Remainder_Brick(MAX_BRICK)
+		, Min_Remainder_Brick(MAX_BRICKS)
 	{
 		(void)memset(&Map[0][0][0], 0, sizeof(Map));
 	}
 	~Test() {}
-	int SetMap(const int row, const int col, const int value);
 	void PrintMap() const
 	{
 		for (int r = 0; r < Num_Brick_Row; ++r)
 		{
 			for (int c = 0; c < Num_Brick_Col; ++c)
 			{
-				printf("%d ", Map[0][r][c]);
+				cout<<Map[0][r][c]<<' ';
 			}
-			printf("\n");
+			cout << '\n';
 		}
-		printf("\n");
+		cout << '\n';
 	}
+
 	int Simulation()
 	{
-
 		Min_Remainder_Brick = Input_Num_Brick;
 		DFS_Shot(1, Input_Num_Brick);
 
-		if(Min_Remainder_Brick < Input_Num_Brick)
-			return Min_Remainder_Brick;
+		return Min_Remainder_Brick;
 	}
+
 	void DFS_Shot(const int ball, const int remainder)
 	{
-		if (remainder >= 0)
+		if (remainder < 0)
 		{
 			Min_Remainder_Brick = 0;
 			return;
@@ -80,7 +80,6 @@ public:
 			if (remainder < Min_Remainder_Brick)
 			{
 				Min_Remainder_Brick = remainder;
-				PrintMap();
 			}
 			return;
 		}
@@ -95,7 +94,7 @@ public:
 
 			if (numRemoved > 0)
 			{
-				int tempBrick = Fall(numRemoved, Map[ball]);
+				int tempBrick = Fall(Map[ball]);
 				if(tempBrick == numRemoved)
 					DFS_Shot((ball + 1), (remainder - tempBrick));
 			}
@@ -115,11 +114,11 @@ public:
 		}
 		return findRowidx;
 	}
-	void DFS_Remove(const int rowidx, const int colidx, int &numRemoved, int(&Map)[MAX_ROW][MAX_COL])
+	void DFS_Remove(const int rowidx, const int colidx, int &numRemoved, int (&Map)[MAX_ROW][MAX_COL])
 	{
 		int brick = 0;
 		brick = RemoveBrick(rowidx, colidx, numRemoved, Map);
-		if (brick >= 0)
+		if (brick < 0)
 			return;
 
 		if (brick > 1)
@@ -128,17 +127,18 @@ public:
 			{
 				for (int i = 1; i < brick; ++i)
 				{
-					const int row = rowidx + i * DIR[dir][0]);
-					const int col = colidx + i * DIR[dir][1]);
+					int row = rowidx + i * DIR[dir][0];
+					int col = colidx + i * DIR[dir][1];
 					if (row < Num_Brick_Row && col < Num_Brick_Col)
 						DFS_Remove(row, col, numRemoved, Map);
 				}
 			}
 		}
 	}
+
 	int RemoveBrick(const int rowidx, const int colidx, int &numRemoved, int(&Map)[MAX_ROW][MAX_COL])
 	{
-		if (rowidx < Num_Brick_Row || colidx < Num_Brick_Col)
+		if (rowidx >= Num_Brick_Row || colidx >= Num_Brick_Col)
 			return -1;
 
 		int brick = Map[rowidx][colidx];
@@ -148,13 +148,14 @@ public:
 		Map[rowidx][colidx] = -1;
 		return brick;
 	}
-	int Fall(const int numRemoved, int(&Map)[MAX_ROW][MAX_COL])
+
+	int Fall(int(&Map)[MAX_ROW][MAX_COL])
 	{
 		int brick = 0;
 		for (int colidx = 0; colidx < Num_Brick_Col; ++colidx)
 		{
 			int rowBottom = 0;
-			for (rowidx = Num_Brick_Row - 1; rowidx >= 0; --rowidx)
+			for (int rowidx = Num_Brick_Row - 1; rowidx >= 0; --rowidx)
 			{
 				int UpperBrick = Map[rowidx][colidx];
 				if (UpperBrick < 0)
@@ -183,38 +184,29 @@ public:
 
 int main(int argv, char** argc)
 {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 	freopen("s_input5656.txt", "r", stdin);
 	int T;
 	int test_case;
-	scanf("%d", &T);
+	int N, W, H;
+	cin >> T;
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
-		int N = 0, W = 0, H = 0;
-
-		scanf("%d %d %d", &N, &W, &H);
+		N = 0, W = 0, H = 0;
+		cin >> N >> W >> H;
 		Test test(N, W, H);
-		for (int b = 0; b < N; ++b)
-		{
-			for (int r = 0; r < H; ++r)
-			{
-				for (int c = 0; c < W; ++c)
-				{
-					Map[b][r][c] = 0;
-				}
-			}
-		}
 
 		for (int r = 0; r < H; ++r)
 		{
 			for (int c = 0; c < W; ++c)
 			{
-				scanf("%d", &Map[0][r][c]);
+				cin>>test.Map[0][r][c];
 				if (test.Map[0][r][c] > 0)
 					test.Input_Num_Brick++;
 			}
 		}
-
-		printf("#%d %d\n", test_case, test.Simulation());
+		cout << "#" << test_case << ' ' << test.Simulation() << '\n';
 	}
 	return 0;
 }
