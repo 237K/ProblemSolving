4e21e72ce9363565f9ff60b19ce60b0719736402
237K <237ref@gmail.com>
2019-03-21 13:44:18 +0900

    [SAMSUNG SWEA] <#5656> (모의) ver2

--- SAMSUNG_SWEA#5656_ver2.cpp
+++ SAMSUNG_SWEA#5656_ver2.cpp
@@ -11,6 +11,7 @@
 #pragma GCC optimize("O3")
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <algorithm>
 #include <memory.h>
 using namespace std;
 
@@ -24,8 +25,92 @@ const static int DIR[DIRECT][2] = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
 
 static int Map[MAX_BALL][MAX_ROW][MAX_COL];
 static int N, W, H;
+static int Input_Brick;
 static pair<coor, int> Q[MAX_ROW * MAX_COL];
+int Qstart, Qend;
 
+inline int Shot(int ball)
+{
+	int result = 0, temp = 0, curx = 0, cury = 0;
+
+	if (ball > N)
+		return 0;
+	
+	(void)memcpy(&Map[ball][0][0], &Map[ball - 1][0][0], sizeof(Map));
+
+	for (int c = 0; c < W; ++c)
+	{
+		temp = 0;
+		bool check1 = true;
+		bool check2 = false;
+		for (int r = 0; r < H; ++r)
+		{
+			if (Map[ball][r][c] > 0)
+			{
+				check1 = false;
+				if (Map[ball][r][c] > 1)
+					check2 = true;
+				Qstart = 0;
+				Qend = 0;
+				Q[Qend++] = { {r, c}, Map[ball][r][c] };
+				Map[ball][r][c] = 0;
+				temp++;
+				while (Qstart < Qend)
+				{
+					int x = Q[Qstart].first.first;
+					int y = Q[Qstart].first.second;
+					int size = Q[Qstart].second;
+
+					if (Map[ball][x][y] == 1)
+						continue;
+					for (int d = 0; d < DIRECT; ++d)
+					{
+						for (int s = 1; s < size; ++s)
+						{
+							curx = x + (1 * DIR[d][0]);
+							cury = y + (1 * DIR[d][1]);
+							if (curx < 0 || cury < 0 || curx >= H || cury >= W)
+								break;
+							if (Map[ball][x][y] == 0)
+								continue;
+							if (Map[ball][x][y] > 1)
+							{
+								Q[Qend++] = { {curx, cury}, Map[ball][curx][cury] };
+								Map[ball][curx][cury] = 0;
+								temp++;
+							}
+						}
+					}
+				}
+				break;
+			}
+		}
+		if (check1)
+			continue;
+		if (check2)
+		{
+			for (int c = 0; c < W; ++c)
+			{
+				int drop = 0;
+				for (int r = H - 1; r >= 0; --r)
+				{
+					if (Map[ball][r][c] == 0)
+						drop++;
+					else if (drop)
+					{
+						curx = r + drop;
+						
+						int swap_container = Map[ball][r][c];
+						Map[ball][r][c] = Map[ball][curx][c];
+						Map[ball][curx][c] = swap_container;
+					}
+				}
+			}
+		}
+		result = max(result, Shot(ball + 1) + temp);
+	}
+	return result;
+}
 
 int main(int argv, char** argc)
 {
@@ -36,25 +121,22 @@ int main(int argv, char** argc)
 	int test_case;
 	int N, W, H;
 	cin >> T;
-	//scanf("%d", &T);
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
 		N = 0, W = 0, H = 0;
 		cin >> N >> W >> H;
-		//scanf("%d %d %d", &N, &W, &H);
 
 		for (int r = 0; r < H; ++r)
 		{
 			for (int c = 0; c < W; ++c)
 			{
-				cin >> test.Map[0][r][c];
-				//scanf("%d", &test.Map[0][r][c]);
-				if (test.Map[0][r][c] > 0)
-					test.Input_Num_Brick++;
+				cin >> Map[0][r][c];
+				if (Map[0][r][c] > 0)
+					Input_Brick++;
 			}
 		}
-		cout << "#" << test_case << ' ' << test.Simulation() << '\n';
-		//printf("#%d %d\n", test_case, test.Simulation());
+		int Result = Input_Brick - Shot(1);
+		cout << "#" << test_case << ' ' << Result << '\n';
 	}
 	return 0;
 }
