b9e80012f02ac049ab51ed11c928f6f9d6c5cc46
237K <237ref@gmail.com>
2019-04-02 17:00:14 +0900

    [SAMSUNG SWEA] <#5653> (모의) 줄기세포배양 ver1

--- SAMSUNG_SWEA#5653.cpp
+++ SAMSUNG_SWEA#5653.cpp
@@ -16,58 +16,115 @@ using namespace std;
 typedef pair<int, int> coor;
 typedef pair<coor, int> cell;
 
-const static int MAP_SIZE = 300;
+const static int MAP_SIZE = 310;
 const static int DIRECT = 4;
+const static int COSMOLOGICAL_CONSTANT = 237;
 
 static int N, M, K;
-static int map[MAP_SIZE * 2 + 51][MAP_SIZE * 2 + 51];
+static int org_map[MAP_SIZE * 2 + 51][MAP_SIZE * 2 + 51];
+static int life_map[MAP_SIZE * 2 + 51][MAP_SIZE * 2 + 51];
+static int dead_map[MAP_SIZE * 2 + 51][MAP_SIZE * 2 + 51];
 static int dir[DIRECT][2] = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
 static priority_queue<cell> PQ;
 static queue<cell> Q;
 
-inline int tiktok()
+inline void Print()
 {
-	int sleep = 0;
-	int wakeup = 0;
-
-	while (!PQ.empty())
+	for (register int r = 300; r < MAP_SIZE + 50; ++r)
 	{
+		for (register int c = 300; c < MAP_SIZE + 50; ++c)
+		{
+			cout << org_map[r][c] << ' ';
+		}
+		cout << "\n\n";
+	}
+}
+
+inline int Count()
+{
+	int all_cell = 0;
+	int live_cell = 0;
+	int dead_cell = 0;
+	for (register int r = 0; r < MAP_SIZE * 2 + 51; ++r)
+	{
+		for (register int c = 0; c < MAP_SIZE * 2 + 51; ++c)
+		{
+			if (life_map[r][c] > (COSMOLOGICAL_CONSTANT - 10))
+				live_cell++;
+			if (org_map[r][c] > 0)
+				all_cell++;
+			if (life_map[r][c] < 0)
+				dead_cell++;
+		}
+	}
+	return all_cell - dead_cell;
+}
+
+inline void tiktok()
+{
+	int dead_cell = 0;
+	for (register int r = 0; r < MAP_SIZE * 2 + 51; ++r)
+	{
+		for (register int c = 0; c < MAP_SIZE * 2 + 51; ++c)
+		{
+			if (life_map[r][c] > 0)
+			{
+				life_map[r][c]--;
+				if (life_map[r][c] == COSMOLOGICAL_CONSTANT)
+				{
+					PQ.push({ {r, c}, org_map[r][c] });
+				}
+				if (life_map[r][c] == dead_map[r][c])
+				{
+					life_map[r][c] = -(COSMOLOGICAL_CONSTANT);
+					dead_cell++;
+				}
+				
+			}
+		}
+	}
+}
+
+inline void rise()
+{
+	while (!PQ.empty())
+	{ 
 		Q.push(PQ.top());
 		PQ.pop();
 	}
-	while (K--)
+	while (!Q.empty())
 	{
-		int tik = Q.size();
-		while (tik--)
+		cell cur_cell = Q.front();
+		Q.pop();
+		for (register int d = 0; d < DIRECT; ++d)
 		{
-			cell cur_cell = Q.front();
-			Q.pop();
+			int x = cur_cell.first.first + dir[d][0];
+			int y = cur_cell.first.second + dir[d][1];
 			int life = cur_cell.second;
 
-			if (life-- == 0)
+			if (org_map[x][y])
+				continue;
+			else if (!org_map[x][y])
 			{
-				if (cur_cell.second - 1 > K)
-					sleep++;
-				for (register int d = 0; d < DIRECT; ++d)
-				{
-					int x = cur_cell.first.first + dir[d][0];
-					int y = cur_cell.first.second + dir[d][1];
-
-					if (map[x][y])
-						continue;
-					else if(!map[x][y])
-					{
-						map[x][y] = cur_cell.second;
-						Q.push({ {x, y}, map[x][y] });
-					}
-				}
+				org_map[x][y] = life;
+				life_map[x][y] = life + COSMOLOGICAL_CONSTANT + 1;
+				dead_map[x][y] = COSMOLOGICAL_CONSTANT - (life * 2);
 			}
-			else if(life >= 0)
-				Q.push({ {cur_cell.first.first, cur_cell.first.second}, life });
 		}
 	}
-	wakeup = Q.size();
-	return sleep + wakeup;
+}
+
+inline int Execution()
+{
+	int answer = 0;
+	while (K--)
+	{
+		rise();
+		answer = Count();
+		tiktok();
+	}
+	Print();
+	return Count();
 }
 
 int main(int argc, char** argv)
@@ -80,18 +137,23 @@ int main(int argc, char** argv)
 	cin >> T;
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
-		(void)memset(map, 0, sizeof(map));
+		(void)memset(org_map, 0, sizeof(org_map));
+		(void)memset(life_map, 0, sizeof(life_map));
+		(void)memset(dead_map, 0, sizeof(dead_map));
 		cin >> N >> M >> K;
 		for (register int r = MAP_SIZE; r < MAP_SIZE + N; ++r)
 		{
 			for (register int c = MAP_SIZE; c < MAP_SIZE + M; ++c)
 			{
-				cin >> map[r][c];
-				if (map[r][c])
-					PQ.push({ {r, c}, map[r][c] });
+				cin >> org_map[r][c];
+				if (org_map[r][c])
+				{
+					life_map[r][c] = org_map[r][c] + COSMOLOGICAL_CONSTANT;
+					dead_map[r][c] = COSMOLOGICAL_CONSTANT - (org_map[r][c] * 2);
+				}
 			}
 		}
-		cout << "#" << test_case << ' ' << tiktok()<<'\n';
+		cout << "#" << test_case << ' ' << Execution()<<'\n';
 	}
 	return 0;
 }
