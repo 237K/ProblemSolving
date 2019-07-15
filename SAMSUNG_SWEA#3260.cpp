d85513a8ff227aae279902e14c5fa51b418697ec
237K <237ref@gmail.com>
2019-07-15 14:10:24 +0900

    [SAMSUNG SWEA] <#3260> (D3) 두 수의 덧셈

--- SAMSUNG_SWEA#3260.cpp
+++ SAMSUNG_SWEA#3260.cpp
@@ -10,13 +10,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
-static string A, B, tmp;
+static string A, B, tmp, ret;
 int main(int argc, char** argv)
 {
 	freopen("s_input4261.txt", "r", stdin);
 	ios::sync_with_stdio(false); cin.tie(NULL);
-	int T, len, len1, len2;
-	register int tc, n, idx;
+	int T, slen, llen, len1, len2;
+	register int tc, n, idx, i, tmp, upper;
 	cin >> T;
 	for (tc = 1; tc <= T; ++tc)
 	{
@@ -27,10 +27,30 @@ int main(int argc, char** argv)
 			tmp = A;
 			A = B;
 			B = tmp;
-			len = len2;
+			llen = len2;
+			slen = len1;
 		}
-		else len = len1;
-
+		else
+		{
+			llen = len1;
+			slen = len2;
+		}
+		tmp = upper = 0;
+		while (1)
+		{
+			if (slen >= 0) tmp = upper + (A[llen - 1] - '0') + (B[slen - 1] - '0');
+			else tmp = upper + (A[llen - 1] - '0');
+			upper = tmp / 10;
+			tmp %= 10;
+			char cur = tmp + '0';
+			ret.push_back(cur);
+			llen--; slen--;
+			if (llen < 0) break;
+		}
+		idx = ret.length() - 1;
+		cout << "#" << tc << ' ';
+		for (i = idx; i >= 0; --i) cout << ret[i];
+		cout << '\n';
 	}
 	return 0;
 }
