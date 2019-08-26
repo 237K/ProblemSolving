9605207cbe34a1d8580724c9b6f42c42d55dcb58
237K <237ref@gmail.com>
2019-08-26 17:17:41 +0900

    [구현] <BOJ #1001> A-B

---
+++ BOJ#1001.c
@@ -0,0 +1,17 @@
+//
+//	OS Windows
+//	2019.08.25
+//
+//	[Algorithm Problem Solving]
+//
+//	BAEKJOON #1001	A-B
+//
+
+#include <stdio.h>
+int main(void)
+{
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d", A - B);
+	return 0;
+}
