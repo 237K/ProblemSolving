360a6e697e7cbd88089ae75cd76c05aaab038b28
237K <237ref@gmail.com>
2019-08-30 14:00:51 +0900

    [SAMSUNG SWEA] <#1938> (D1) 아주 간단한 계산기

---
+++ SAMSUNG_SWEA#1938.c
@@ -0,0 +1,20 @@
+//
+//	OS Windows
+//	2019.08.29
+//
+//	[Algorithm Problem Solving]
+//
+//	SAMSUNG SW Expert Academy [#1938] (D1) 아주 간단한 계산기
+//
+
+#include <stdio.h>
+int main(void)
+{
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d\n", A + B);
+	printf("%d\n", A - B);
+	printf("%d\n", A * B);
+	printf("%d", A / B);
+	return 0;
+}
