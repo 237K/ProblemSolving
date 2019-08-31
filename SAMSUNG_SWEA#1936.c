770d26e1b1c485b248bfe2796731b471348e6fcc
237K <237ref@gmail.com>
2019-08-31 15:23:09 +0900

    [SAMSUNG SWEA] <#1936> (D1) 1대1 가위바위보

---
+++ SAMSUNG_SWEA#1936.c
@@ -0,0 +1,18 @@
+//
+//	OS Windows
+//	2019.08.30
+//
+//	[Algorithm Problem Solving]
+//
+//	SAMSUNG SW Expert Academy [#1936] (D1) 1대1 가위바위보
+//
+
+#include <stdio.h>
+int main(void)
+{
+	int A, B;
+	scanf("%d %d", &A, &B);
+	if (A > B) printf("A");
+	else printf("B");
+	return 0;
+}
