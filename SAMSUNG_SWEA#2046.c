68857383df146bce745221cb10180dc6d61538fe
237K <237ref@gmail.com>
2019-09-02 12:34:21 +0900

    [SAMSUNG SWEA] <#2046> (D1) 스탬프 찍기

---
+++ SAMSUNG_SWEA#2046.c
@@ -0,0 +1,17 @@
+//
+//	OS Windows
+//	2019.08.29
+//
+//	[Algorithm Problem Solving]
+//
+//	SAMSUNG SW Expert Academy [#2046] (D1) ½ºÅÆÇÁ Âï±â
+//
+
+#include <stdio.h>
+int main(void)
+{
+	int N, n;
+	scanf("%d", &N);
+	for (n = 0; n < N; ++n) printf("#");
+	return 0;
+}
