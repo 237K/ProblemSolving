08dfe78d242159d66e83189b8f1021c76fedb48a
237K <237ref@gmail.com>
2019-09-06 14:09:11 +0900

    [SAMSUNG SWEA] <#2043> (D1) 서랍의 비밀번호

---
+++ SAMSUNG_SWEA#2043.c
@@ -0,0 +1,17 @@
+//
+//	OS Windows
+//	2019.09.02
+//
+//	[Algorithm Problem Solving]
+//
+//	SAMSUNG SW Expert Academy [#2043] (D1) 서랍의 비밀번호
+//
+
+#include <stdio.h>
+int main(void)
+{
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d", A - B + 1);
+	return 0;
+}
