e2a310149ceb1e190873b5d355e1aee60dbff95f
237K <237ref@gmail.com>
2019-09-20 12:59:50 +0900

    [구현] <BOJ #2742> 기찍N

---
+++ BOJ#2742.c
@@ -0,0 +1,17 @@
+//
+//	OS Windows
+//	2019.09.16
+//
+//	[Algorithm Problem Solving]
+//
+//	BAEKJOON #2742 ±âÂï N
+//
+
+#include <stdio.h>
+int main(void)
+{
+	int N, i;
+	scanf("%d", &N);
+	for (i = N; i >= 1; --i) printf("%d\n", i);
+	return 0;
+}
