4e8d27a7f50c93dd734b971cb7a8862b46c09e95
237K <237ref@gmail.com>
2019-10-13 16:35:52 +0900

    [시뮬레이션] <BOJ #17135> 캐슬 디펜스 수정중..

--- BOJ#17135.cpp
+++ BOJ#17135.cpp
@@ -12,13 +12,13 @@
 using namespace std;
 const int ARCHER = 3;
 const int SZ = 16;
-static int archer[ARCHER], check[ARCHER];
+static int archer[ARCHER], check[ARCHER], target_dist[ARCHER], target_r[ARCHER], target_c[ARCHER];
 static int map[SZ][SZ], map_cpy[SZ][SZ];
 static int N, M, D, ret;
 inline void print()
 {
+	cout << "============================================\n\n";
 	register int r, c;
-	cout << "==========================================\n\n";
 	for (r = 0; r < N; ++r)
 	{
 		for (c = 0; c < M; ++c)
@@ -27,51 +27,57 @@ inline void print()
 		}
 		cout << "\n\n";
 	}
-	cout << "==========================================\n\n";
+	cout << "============================================\n\n";
 }
 inline int simul()
 {
-	register int t, r, c, d, a, ar, ac, flag, cnt = 0;
+	register int init, t, r, c, d, dr, dc, a, ar, ac, flag, cnt = 0;
 	for (t = N; t > 0; --t)
 	{
-	
-		cout << "archer location : ";
-		for (register int i = 0; i < 3; ++i) cout << archer[i] << ' ';
-		cout << "\n\n*turn "<<t<<"\n\n";
-		print();
-
-
 		flag = 3;
 		(void)memset(check, 0, sizeof(check));
-		for(r = t-1 ; r >= t - 1 - D ; --r)
+		(void)memset(target_r, -1, sizeof(target_r));
+		(void)memset(target_c, -1, sizeof(target_c));
+		for (init = 0; init < ARCHER; ++init) target_dist[init] = 100;
+
+		for (r = t - 1; r >= t - 1 - D; --r)
 		{
 			if (r < 0) break;
-			if (!flag) break;
 			for (c = 0; c < M; ++c)
 			{
 				if (map[r][c])
 				{
 					for (a = 0; a < ARCHER; ++a)
 					{
-						if (!check[a])
+						ar = t;
+						ac = archer[a];
+						dr = ar - r;
+						dc = ac - c;
+						if (dr < 0) dr *= -1;
+						if (dc < 0) dc *= -1;
+						d = dr + dc;
+						if (d <= D && d <= target_dist[a])
 						{
-							ar = t;
-							ac = archer[a];
-							d = (ar - r) + (ac - c);
-							if (d < 0) d *= -1;
-							if (d <= D)
-							{
-								check[a] = 1;
-								flag--;
-								cnt++;
-								map[r][c] = 0;
-							}
+							if (target_c[a] >= 0 && target_c[a] < c) continue;
+							target_dist[a] = d;
+							target_r[a] = r;
+							target_c[a] = c;
 						}
 					}
 				}
 			}
 		}
-		print();
+		for (a = 0; a < ARCHER; ++a)
+		{
+			r = target_r[a];
+			c = target_c[a];
+			if (r == -1 || c == -1) continue;
+			if (map[r][c])
+			{
+				map[r][c] = 0;
+				cnt++;
+			}
+		}
 	}
 	return cnt;
 }
@@ -80,6 +86,8 @@ inline void set_loca(int cnt, int idx)
 	if (cnt == ARCHER)
 	{
 		(void)memcpy(map, map_cpy, sizeof(map));
+		//cout << "archer location : ";
+		//for (register int p = 0; p < 3; ++p) cout << archer[p] << ' ';
 		int tmp = simul();
 		if (ret < tmp) ret = tmp;
 		return;
