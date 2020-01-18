169f09a50dc64d8959d86eb5be247f18512572af
237K <237ref@gmail.com>
2020-01-18 19:35:14 +0900

    [구현] <BOJ #2566> 최댓값

---
+++ BOJ#2566.c
@@ -0,0 +1,30 @@
+//
+//	OS Windows
+//	2020.01.06
+//
+//	[Algorithm Problem Solving]
+//
+//	[±¸Çö]	<BOJ #2566>	ÃÖ´ñ°ª
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define SZ 10
+int main(void)
+{
+	int cur, r, c, m = 0, mr, mc;
+	for (r = 1; r < SZ; ++r)
+	{
+		for (c = 1; c < SZ; ++c)
+		{
+			scanf("%d", &cur);
+			if (m < cur)
+			{
+				m = cur;
+				mr = r;
+				mc = c;
+			}
+		}
+	}
+	printf("%d\n%d %d", m, mr, mc);
+	return 0;
+}
