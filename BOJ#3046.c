e87cca72c2c3a53dcaa78631ad568dd1bc4313ba
237_macbook <237ref@gmail.com>
2020-01-26 09:47:05 +0900

    [구현] <BOJ #3046> R2

---
+++ BOJ#3046.c
@@ -0,0 +1,17 @@
+//
+//	OS Windows
+//	2020.01.21
+//
+//	[Algorithm Problem Solving]
+//
+//	[±¸Çö]	<BOJ #3046>	R2
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int R1, S;
+	scanf("%d %d", &R1, &S);
+	printf("%d", S * 2 - R1);
+	return 0;
+}
