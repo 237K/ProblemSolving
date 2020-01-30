90aa9136f9fe8dedd3dd4460d874ec587f418b02
237K <237ref@gmail.com>
2020-01-30 19:57:16 +0900

    [구현] <BOJ #2743> 단어 길이 재기

---
+++ BOJ#2743.cpp
@@ -0,0 +1,19 @@
+//
+//	OS Windows
+//	2020.01.20
+//
+//	[Algorithm Problem Solving]
+//
+//	[구현]	<BOJ #2743>	단어 길이 재기
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char** argv)
+{
+	string str;
+	cin >> str;
+	cout << str.length();
+	return 0;
+}
