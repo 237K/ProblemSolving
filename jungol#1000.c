e49c3496c23e9c2e34e4993082ff6ff1613de0a4
237K <237ref@gmail.com>
2020-02-04 13:43:59 +0900

    [구현] <JUNGOL#1000> 두 정수 더하기

---
+++ jungol#1000.c
@@ -0,0 +1,17 @@
+//
+//	OS Windows
+//	2020.02.03
+//
+//	[Algorithm Problem Solving]
+//
+//	[구현] <jungol#1000> 두 정수 더하기
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a + b);
+	return 0;
+}
