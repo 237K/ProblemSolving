8b828357d5d56ed05fcc9603b9e88db98e710a4c
237K <237ref@gmail.com>
2020-02-17 00:05:41 +0900

    [구현] <jungol #1051> 평균값

---
+++ jungol#1051.c
@@ -0,0 +1,17 @@
+//
+//	OS Windows
+//	2020.02.10
+//
+//	[Algorithm Problem Solving]
+//
+//	[±¸Çö] <jungol#1051> Æò±Õ°ª
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int R1, S;
+	scanf("%d %d", &R1, &S);
+	printf("%d", 2 * S - R1);
+	return 0;
+}
