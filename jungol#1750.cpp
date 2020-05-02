33895255bfe606f152a720e07a3aaaad180f5d80
237K <237ref@gmail.com>
2020-05-02 13:02:25 +0900

    [시뮬레이션] <jungol #1750> 섬의 침몰

--- jungol#1750.cpp
+++ jungol#1750.cpp
@@ -17,7 +17,7 @@ const static int MAX = SZ * SZ;
 const static int DIR = 4;
 static int dir[DIR][2] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
 static pii small_lake[MAX];
-static int map[SZ][SZ], check[SZ][SZ], num_small_lake, R, C, connect_flag;
+static int map[SZ][SZ], check[SZ][SZ], num_small_lake, num_ground, R, C, connect_flag;
 inline void print()
 {
 	register int r, c;
@@ -32,10 +32,22 @@ inline void print()
 	}
 	cout << "=========================================\n\n";
 }
+inline void print_small_lake()
+{
+	register int i, cr, cc;
+	for (i = 0; i < num_small_lake; ++i)
+	{
+		cr = small_lake[i].first;
+		cc = small_lake[i].second;
+		cout << "(" << cr << ", " << cc << ") ";
+	}
+	cout << "\n\n";
+}
 inline void flooding()
 {
-	register int i, r, c, d, nr, nc, cnt, num_ToBeLocked = 0;
+	register int i, r, c, d, nr, nc, cnt, num_ToBeLocked;
 	pii ToBeLocked[MAX];
+	num_ToBeLocked = 0;
 	for (r = 0 ; r < R ; ++r)
 	{
 		for (c = 0; c < C; ++c)
@@ -43,10 +55,12 @@ inline void flooding()
 			if (map[r][c] == 1)
 			{
 				cnt = 0;
+				//cout << "check : (" << r << ", " << c << ")\n";
 				for (d = 0; d < DIR; ++d)
 				{
 					nr = r + dir[d][0];
 					nc = c + dir[d][1];
+					//cout << "(" << nr << ", " << nc << ")\n";
 					if (nr < 0 || nc < 0 || nr >= R || nc >= C)
 						continue;
 					if (!connect_flag)
@@ -60,9 +74,10 @@ inline void flooding()
 							cnt++;
 					}
 				}
+				//cout << "cnt : " << cnt << "\n";
 				if (cnt >= 2)
 				{
-					ToBeLocked[num_ToBeLocked++] = { nr, nc };
+					ToBeLocked[num_ToBeLocked++] = { r, c };
 				}
 			}
 		}
@@ -70,11 +85,12 @@ inline void flooding()
 	for (i = 0; i < num_ToBeLocked; ++i)
 	{
 		map[ToBeLocked[i].first][ToBeLocked[i].second] = 2;
+		num_ground--;
 	}
 }
 inline int check_end()
 {
-	register int r, c, flag = 1;
+	/*register int r, c, flag = 1;
 	for (r = 0; r < R; ++r)
 	{
 		for (c = 0; c < C; ++c)
@@ -87,38 +103,30 @@ inline int check_end()
 		}
 		if (!flag) break;
 	}
+	return flag;*/
+	int flag;
+	if (!num_ground) flag = 1;
+	else flag = 0;
 	return flag;
 }
 inline void check_connect()
 {
-	register int i, r, c, cr, cc, d, nr, nc;
-	queue<pii> Q;
-	while (!Q.empty()) { Q.pop(); }
-	(void)memset(check, 0, sizeof(check));
+	register int i, cr, cc, d, nr, nc;
 	for (i = 0; i < num_small_lake; ++i)
 	{
-		r = small_lake[i].first;
-		c = small_lake[i].second;
-		Q.push({ r, c });
-		check[r][c] = 1;
-		while (!Q.empty())
+		cr = small_lake[i].first;
+		cc = small_lake[i].second;
+		for (d = 0; d < DIR; ++d)
 		{
-			cr = Q.front().first;
-			cc = Q.front().second;
-			Q.pop();
-			for (d = 0; d < DIR; ++d)
+			nr = cr + dir[d][0];
+			nc = cc + dir[d][1];
+			if (nr < 0 || nc < 0 || nr >= R || nc >= C)
+				continue;
+			if (map[nr][nc] == 2)
 			{
-				nr = cr + dir[d][0];
-				nc = cc + dir[d][1];
-				if (nr < 0 || nc < 0 || nr >= R || nc >= C || check[nr][nc] || map[nr][nc] == 1)
-					continue;
-				if (map[nr][nc] == 2)
-				{
-					connect_flag = 1;
-					break;
-				}
-				Q.push({ nr, nc });
-				check[nr][nc] = 1;
+				//cout << "("<<nr<<", "<<nc<<") connect!\n\n";
+				connect_flag = 1;
+				break;
 			}
 		}
 		if (connect_flag) break;
@@ -177,15 +185,18 @@ inline int simul()
 {
 	connect_flag = 0;
 	find_small_lake();
+	//print();
+	//print_small_lake();
 	register int cnt = 0;
 	while (1)
 	{
 		cnt++;
-		cout << cnt << '\n';
-		print();
-		flooding();
 		if (!connect_flag) check_connect();
+		flooding();
 		if (check_end()) break;
+		//cout << cnt << '\n';
+		//cout << "connect_flag : " << connect_flag << "\n\n";
+		//print();
 	}
 	return cnt;
 }
@@ -195,6 +206,7 @@ int main(int argc, char** argv)
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	(void)memset(map, 0, sizeof(map));
+	num_ground = 0;
 	register int r, c;
 	cin >> C >> R;
 	for (r = 0; r < R; ++r)
@@ -202,6 +214,7 @@ int main(int argc, char** argv)
 		for (c = 0; c < C; ++c)
 		{
 			cin >> map[r][c];
+			if (map[r][c]) num_ground++;
 		}
 	}
 	cout << simul();
