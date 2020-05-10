fd95079a2692ce594846585cc2d21223379da8cd
237K <237ref@gmail.com>
2020-05-10 09:13:25 +0900

    [구현] <jungol #1840> 평균

---
+++ jungol#1840.c
@@ -0,0 +1,16 @@
+//
+//	OS Windows
+//	2020.04.30
+//
+//	[Algorithm Problem Solving]
+//
+//	[±¸Çö] <jungol#1849> Æò±Õ
+//
+#define _CRT_SECURE_NO_WARNINGS
+int main(void)
+{
+	int A, M;
+	scanf("%d %d", &A, &M);
+	printf("%d", M * 2 - A);
+	return 0;
+}
